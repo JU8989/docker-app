80d02a5e790aa93a51fb3907653913cdbe8880a8
JU8989 <55870369+JU8989@users.noreply.github.com>
2022-03-01 01:27:33 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Docker!
+          Learn Docker with elastic beanstalk
         </a>
       </header>
     </div>
